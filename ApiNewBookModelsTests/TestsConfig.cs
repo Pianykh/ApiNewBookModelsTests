4fde2f8bc16859e4739812191aeb3138f0d7e655
Sergei Pianykh <pyanykhs@gmail.com>
2021-05-26 16:22:46 +0300

    fixed issue with generation of new user

--- ApiNewBookModelsTests/TestsConfig.cs
+++ ApiNewBookModelsTests/TestsConfig.cs
@@ -8,17 +8,16 @@ namespace ApiNewBookModelsTests
 {
     public class TestsConfig
     {
-        protected static string _uniqueNumber = DateTime.Now.ToString("yyyyMMddhhmmss");
         protected static string _validFirstName = "John";
         protected static string _validSecondName = "Tribiani";
         protected static string _validPassword = "1Qazxcdew46109(";
         protected static string _validNewPassword = "1Qazxcdew46109(@";
-        protected static string _validEmail = $"qweqweqweqweqwe{_uniqueNumber}@gmail.com";
-        protected static string _validNewEmail = $"rtyrtyrtyrtyrty{_uniqueNumber}@gmail.com";
+        protected string _validEmail = $"qweqweqweqweqwe{DateTime.Now.ToString("yyyyMMddhhmmssfffffff")}@gmail.com";
+        protected string _validNewEmail = $"rtyrtyrtyrtyrty{DateTime.Now.ToString("yyyyMMddhhmmssfffffff")}@gmail.com";
         protected static string _validPhone = "1231231212";
         protected static string _validNewFirstName = "Bruce";
         protected static string _validNewSecondName = "Willis";
-        protected static ClientAuthModel _user;
+        protected  ClientAuthModel _user;
         protected static string _validCompanyName = "Italgas";
         protected static string _validCompanySite = "http://Italgas.it";
         protected static string _validCompanyDescription = "ItalGas description";
